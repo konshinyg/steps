4045615824d768467e790427c8750b0201d1c52a
Core <konshinyg@mail.ru>
2017-08-11 03:07:30 +0300

    storyboard added

---
+++ urSteps/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  urSteps
+//
+//  Created by Core on 11.08.17.
+//  Copyright © 2017 Cornelius. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ urSteps/RecViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecViewController.swift
+//  urSteps
+//
+//  Created by Core on 11.08.17.
+//  Copyright © 2017 Cornelius. All rights reserved.
+//
+
+import UIKit
+
+class RecViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
