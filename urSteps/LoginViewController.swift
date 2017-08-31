9dced38c3f0270640cc5b0c1e01e947c90d8f611
Core <konshinyg@mail.ru>
2017-09-01 02:49:01 +0300

    file add

--- urSteps/LoginViewController.swift
+++ urSteps/LoginViewController.swift
@@ -14,12 +14,24 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginDataLabel: UILabel!
     
     @IBAction func enterData(_ sender: UIButton) {
-        let user = "c"
-        let pass = "c"
+        let user1 = "a"
+        let pass1 = "a"
+        let user2 = "b"
+        let pass2 = "b"
+        let user3 = "c"
+        let pass3 = "c"
         
-        if username.text == user && password.text == pass {
+        if username.text == user1 && password.text == pass1 {
             loginDataLabel.text = "Correct data!"
-            loginAccess()
+            loginAccess(tokenstr: user1)
+        }
+        if username.text == user2 && password.text == pass2 {
+            loginDataLabel.text = "Correct data!"
+            loginAccess(tokenstr: user2)
+        }
+        if username.text == user3 && password.text == pass3 {
+            loginDataLabel.text = "Correct data!"
+            loginAccess(tokenstr: user3)
         }
         else {
             loginDataLabel.text = "Incorrect data!"
@@ -28,9 +40,24 @@ class LoginViewController: UIViewController {
         password.resignFirstResponder()
 
     }
-    func loginAccess() {
+    func loginAccess(tokenstr: String) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "tabBarID")
         self.present(vc!, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "LoginSegueID" {
+            let dest = segue.destination
+            if username.text == "user1" {
+            dest.setValue("user1", forKeyPath: "user")
+            }
+            if username.text == "user2" {
+                dest.setValue("user2", forKeyPath: "user")
+            }
+            if username.text == "user3" {
+                dest.setValue("user3", forKeyPath: "user")
+            }
+        }
     }
     
     override func viewDidLoad() {
